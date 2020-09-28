819c05492269bac40832d4026412074657b3ced2
Chinmayee90 <chinmayee.sakharshete@gmail.com>
2020-09-28 12:49:12 +0530

    filter sort redux

--- src/App.js
+++ src/App.js
@@ -22,43 +22,43 @@ class App extends React.Component {
     };
   }
 
-  sortProducts = (event) => {
-    const sort = event.target.value;
-    this.setState({
-      sort,
-      products: this.state.products
-        .slice()
-        .sort((a, b) =>
-          sort === "lowest"
-            ? a.price > b.price
-              ? 1
-              : -1
-            : sort === "highest"
-            ? a.price < b.price
-              ? 1
-              : -1
-            : a._id < b._id
-            ? 1
-            : -1
-        ),
-    });
-  };
+  // sortProducts = (event) => {
+  //   const sort = event.target.value;
+  //   this.setState({
+  //     sort,
+  //     products: this.state.products
+  //       .slice()
+  //       .sort((a, b) =>
+  //         sort === "lowest"
+  //           ? a.price > b.price
+  //             ? 1
+  //             : -1
+  //           : sort === "highest"
+  //           ? a.price < b.price
+  //             ? 1
+  //             : -1
+  //           : a._id < b._id
+  //           ? 1
+  //           : -1
+  //       ),
+  //   });
+  // };
 
-  filterProducts = (event) => {
-    if (event.target.value === "") {
-      this.setState({
-        size: event.target.value,
-        products: data.products,
-      });
-    } else {
-      this.setState({
-        size: event.target.value,
-        products: data.products.filter((product) => {
-          return product.availableSizes.indexOf(event.target.value) >= 0;
-        }),
-      });
-    }
-  };
+  // filterProducts = (event) => {
+  //   if (event.target.value === "") {
+  //     this.setState({
+  //       size: event.target.value,
+  //       products: data.products,
+  //     });
+  //   } else {
+  //     this.setState({
+  //       size: event.target.value,
+  //       products: data.products.filter((product) => {
+  //         return product.availableSizes.indexOf(event.target.value) >= 0;
+  //       }),
+  //     });
+  //   }
+  // };
 
   addToCart = (product) => {
     const cartItems = this.state.cartItems.slice(); //clone
@@ -102,17 +102,8 @@ class App extends React.Component {
           <main>
             <div className="content">
               <div className="main">
-                <Filter
-                  count={this.state.products.length}
-                  size={this.state.size}
-                  sort={this.state.sort}
-                  filterProducts={this.filterProducts}
-                  sortProducts={this.sortProducts}
-                ></Filter>
-                <Products
-                  products={this.state.products}
-                  addToCart={this.addToCart}
-                ></Products>
+                <Filter></Filter>
+                <Products addToCart={this.addToCart}></Products>
               </div>
               <div className="sidebar">
                 <Cart
--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,4 +1,8 @@
-import { FETCH_PRODUCTS } from "../types";
+import {
+  FETCH_PRODUCTS,
+  FILTER_PRODUCTS_BY_SIZE,
+  ORDER_PRODUCTS_BY_PRICE,
+} from "../types";
 
 export const fetchProducts = () => async (dispatch) => {
   const res = await fetch("/api/products"); //call to backend
@@ -6,5 +10,43 @@ export const fetchProducts = () => async (dispatch) => {
   dispatch({
     type: FETCH_PRODUCTS,
     payload: data,
+  });
+};
+
+export const filterProducts = (products, size) => async (dispatch) => {
+  dispatch({
+    type: FILTER_PRODUCTS_BY_SIZE,
+    payload: {
+      size,
+      items:
+        size === ""
+          ? products
+          : products.filter((x) => x.availableSizes.indexOf(size) >= 0),
+    },
+  });
+};
+
+export const sortProducts = (products, sort) => async (dispatch) => {
+  const sortedProducts = products.slice();
+  if (sort === "latest") {
+    sortedProducts.sort((a, b) => (a._id > b._id ? 1 : -1));
+  } else {
+    sortedProducts.sort((a, b) =>
+      sort === "lowest"
+        ? a.price > b.price
+          ? 1
+          : -1
+        : a.price < b.price
+        ? 1
+        : -1
+    );
+  }
+
+  dispatch({
+    type: ORDER_PRODUCTS_BY_PRICE,
+    payload: {
+      sort,
+      items: sortedProducts,
+    },
   });
 };
